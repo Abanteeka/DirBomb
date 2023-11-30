f2445b465e1edede99a15bda6a567b724ac25797
Abanteeka <acharyaabanteeka196@gmail.com>
2023-11-30 22:01:58 +0530

    update main.py

--- main.py
+++ main.py
@@ -10,7 +10,22 @@ def print_hi(name):
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+#if __name__ == '__main__':
+    #print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import pyfiglet
+from termcolor import colored
+import argparse
+
+def banner():
+   banner = colored(pyfiglet.figlet_format('DirBomb', font='slant'), 'yellow')
+   #author
+   print(f"{banner}")
+
+def arguments():
+    args = argparse.ArgumentParser()
+    args.add_argument("-u", "--url", help="Target URL")
+    args.add_argument("-w", "--wordlist", help="Wordlist")
+    arg = args.parse_args()
+
